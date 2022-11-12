4dcf925b805375ede5a1c2517a20e284dfb28e28
d1psy <niki.nice1203@gmail.com>
2022-11-12 18:06:39 +0100

    add(settings): added some settings (not working at the moment)

--- v1/keybinds.go
+++ v1/keybinds.go
@@ -1,7 +1,7 @@
 package v1
 
 import (
-    "github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/key"
 )
 
 // all the keybindings
@@ -11,20 +11,34 @@ type ListKeyMap struct {
     changeEditor key.Binding
 }
 
-// all the key functions
-func newListKeyMap() *ListKeyMap {
+// holds the key as a char (string) and the desc as a string
+type KeyMap struct {
+    key string
+    desc string
+}
+
+// KeyMaps struct
+type KeyMaps struct {
+    deleteItem KeyMap
+    addItem KeyMap
+    changeEditor KeyMap
+}
+
+// TODO: deserialisation function for keymaps
+func newListKeyMap(keyMaps KeyMaps) *ListKeyMap {
 	return &ListKeyMap{
         deleteItem: key.NewBinding(
-        key.WithKeys("D"),
-        key.WithHelp("D", "remove an item"),
+        key.WithKeys(keyMaps.deleteItem.key),
+        key.WithHelp(keyMaps.deleteItem.key, keyMaps.deleteItem.desc),
             ),
         addItem: key.NewBinding(
-        key.WithKeys("A"),
-        key.WithHelp("A", "add an item"),
+        key.WithKeys(keyMaps.addItem.key),
+        key.WithHelp(keyMaps.addItem.key, keyMaps.addItem.desc),
             ),
         changeEditor: key.NewBinding(
-        key.WithKeys("E"),
-        key.WithHelp("E", "change the editor"),
+        key.WithKeys(keyMaps.changeEditor.key),
+        key.WithHelp(keyMaps.changeEditor.key, keyMaps.changeEditor.desc),
             ),
     }
 }
+
